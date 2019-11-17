5178b6aa92258a42a62e5de32fb5245c20d065df
nredjap <hinanias@gmail.com>
2019-11-18 01:31:33 +0900

    06.3 stopボタンでSleepQualityFragmentへの遷移動作を追加

--- TrackMySleepQuality-Starter/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ TrackMySleepQuality-Starter/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -18,6 +18,7 @@ package com.example.android.trackmysleepquality.sleeptracker
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import com.example.android.trackmysleepquality.database.SleepDatabaseDao
@@ -39,6 +40,10 @@ class SleepTrackerViewModel(
         formatNights(it, application.resources)
     }
     private var tonight = MutableLiveData<SleepNight?>()
+
+    private val _navigateToSleepQuality = MutableLiveData<SleepNight>()
+    val navigateToSleepQuality: LiveData<SleepNight>
+        get() = _navigateToSleepQuality
 
     init {
         initializeTonight()
@@ -79,6 +84,7 @@ class SleepTrackerViewModel(
             val oldNight = tonight.value ?: return@launch
             oldNight.endTimeMilli = System.currentTimeMillis()
             update(oldNight)
+            _navigateToSleepQuality.value = oldNight
         }
     }
 
@@ -87,6 +93,11 @@ class SleepTrackerViewModel(
             database.update(night)
         }
     }
+
+    fun doneNavigating() {
+        _navigateToSleepQuality.value = null
+    }
+
 
     fun onClear() {
         uiScope.launch {
