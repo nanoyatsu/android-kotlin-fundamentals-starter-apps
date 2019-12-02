7b8d9052a8ccbc0c15372f555f098cf2ee4b9328
nredjap <hinanias@gmail.com>
2019-12-03 00:39:32 +0900

    03.2 上部オプションメニューを追加（特定fragmentでだけ有効にすることが可能）

--- AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -2,12 +2,11 @@ package com.example.android.navigation
 
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 /**
@@ -23,9 +22,19 @@ class TitleFragment : Fragment() {
         binding.playButton.setOnClickListener {
             it.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
         }
+        setHasOptionsMenu(true)
 
         return binding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.options_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return NavigationUI.onNavDestinationSelected(item!!,
+                view!!.findNavController()) || super.onOptionsItemSelected(item)
+    }
 
 }
