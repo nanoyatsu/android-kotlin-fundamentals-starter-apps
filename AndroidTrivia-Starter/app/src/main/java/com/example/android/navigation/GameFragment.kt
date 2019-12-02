b55e25117410c4fea6f99fa6feda8b5300ff4c22
nredjap <hinanias@gmail.com>
2019-12-03 00:11:22 +0900

    03.2 引き続きNavController.navigate()を使ったFragment遷移（条件付き）（普通にifの先でやるだけ）

--- AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/GameFragment.kt
+++ AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/GameFragment.kt
@@ -23,6 +23,7 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentGameBinding
 
 class GameFragment : Fragment() {
@@ -55,7 +56,6 @@ class GameFragment : Fragment() {
             Question(text = "What do you use to mark a layout for data binding?",
                     answers = listOf("<layout>", "<binding>", "<data-binding>", "<dbinding>"))
     )
-
 
 
     lateinit var currentQuestion: Question
@@ -99,9 +99,11 @@ class GameFragment : Fragment() {
                         binding.invalidateAll()
                     } else {
                         // We've won!  Navigate to the gameWonFragment.
+                        view.findNavController().navigate(R.id.action_gameFragment_to_gameWonFragment)
                     }
                 } else {
                     // Game over! A wrong answer sends us to the gameOverFragment.
+                    view.findNavController().navigate(R.id.action_gameFragment_to_gameOverFragment)
                 }
             }
         }
