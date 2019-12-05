5793a8194971d6b2d8aaaba91b4889da2c0d6aed
nredjap <hinanias@gmail.com>
2019-12-05 23:35:22 +0900

    09.2 requestに制約を追加 内容＞非従量課金回線、バッテリー十分、充電中、デバイスアイドル状態のみ

--- DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/DevByteApplication.kt
+++ DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/DevByteApplication.kt
@@ -17,9 +17,8 @@
 package com.example.android.devbyteviewer
 
 import android.app.Application
-import androidx.work.ExistingPeriodicWorkPolicy
-import androidx.work.PeriodicWorkRequestBuilder
-import androidx.work.WorkManager
+import android.os.Build
+import androidx.work.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -44,7 +43,15 @@ class DevByteApplication : Application() {
     }
 
     private fun setupRecurringWork() {
-        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(1, TimeUnit.DAYS).build()
+        val constraints = Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.UNMETERED)
+                .setRequiresBatteryNotLow(true)
+                .setRequiresCharging(true)
+                .apply { if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) setRequiresDeviceIdle(true) }
+                .build()
+        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(1, TimeUnit.DAYS)
+                .setConstraints(constraints)
+                .build()
         WorkManager.getInstance().enqueueUniquePeriodicWork(
                 RefreshDataWorker.WORK_NAME,
                 ExistingPeriodicWorkPolicy.KEEP,
